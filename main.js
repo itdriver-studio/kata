75f731bb075ebce3ccd4f4ac2cdd7a40757013ac
Dmitrii Ivanov <itdriver@yahoo.com>
2023-03-22 20:17:33 +0100

    Ready css and js

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-const myDog = {
-    "name": "Happy Coder",
-    "legs": 4,
-    "tails": 1,
-    "friends": ["freeCodeCamp Campers"]
-};
+// Setup
+function phoneticLookup(val) {
+    let result = "";
 
-myDog.bark = "woof"
+    // Only change code below this line
+    switch (val) {
+        case "alpha":
+            result = "Adams";
+            break;
+        case "bravo":
+            result = "Boston";
+            break;
+        case "charlie":
+            result = "Chicago";
+            break;
+        case "delta":
+            result = "Denver";
+            break;
+        case "echo":
+            result = "Easy";
+            break;
+        case "foxtrot":
+            result = "Frank";
+    }
+
+    // Only change code above this line
+    return result;
+}
+
+phoneticLookup("charlie");
