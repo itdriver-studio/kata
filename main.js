07a5aaa5144f8df330ed86d38d6b4873b0379c60
Dmitrii Ivanov <itdriver@yahoo.com>
2023-03-22 20:15:26 +0100

    Ready css and js

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-// Setup
-const testObj = {
-    12: "Namath",
-    16: "Montana",
-    19: "Unitas"
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
 };
 
-// Only change code below this line
-const playerNumber = 16; // Change this line
-const player = testObj[playerNumber]; // Change this line
-
-
-console.log(player);
+myDog.bark = "woof"
