26006224b0b967c5adde61089a42849a51e271bd
Dmitrii Ivanov <itdriver@yahoo.com>
2023-03-22 20:57:06 +0100

    Ready css and js

--- main.js
+++ main.js
@@ -1,26 +1,6 @@
-< !DOCTYPE html >
-    <
-    html lang = "en" >
-    <
-    head >
-    <
-    meta charset = "utf-8" / >
-    <
-    title > Cafe Menu < /title> <
-    link href = "styles.css"
-rel = "stylesheet" / >
-    <
-    /head> <
-    body >
-    <
-    main >
-    <
-    h1 > CAMPER CAFE < /h1> <
-    p > Est.2020 < /p> <
-    section >
-    <
-    h2 > Coffee < /h2> <
-    /section> <
-    /main> <
-    /body> <
-    /html>
+body {
+    background - color: brown;
+}
+h1, h2, p {
+    text - align: center;
+}
